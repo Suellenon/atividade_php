c12193d809c3f2915e21baa91da7dce6293dd3f8
suellenon <sullen502@gmail.com>
2024-07-10 16:46:20 -0300

    atv.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+//Q1
+$NOME = 'Maria';
+echo $NOME . "<br>";
+
+//Q2
+$nome = 'João';
+$idade = 34;
+$endereco= 'São Luis';
+$curso = 'barbeiro';
+
+echo "Olá eu sou $nome , tenho $idade anos, moro em $endereco  e faço curso de $curso no Senac ";
+
+//Q3
+// $numero1 = 4
+// $numero2 = 6
+
+
+//Q4
+
+
+
+
+
+//Q5
+
+ 
+   
+  
+//Q6
+
+
+
+
+
+
+
+?>

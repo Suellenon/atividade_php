9ee9aed8fa0d17fd2d5b1bd0456c4fd4feb6a07c
suellenon <sullen502@gmail.com>
2024-07-12 22:56:17 -0300

    todasrespondidas

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
- <?php
+  <?php
     //Q1
     $NOME = 'Maria';
     echo $NOME . "<br>";
@@ -23,6 +23,18 @@
      echo "A subtração de $valor1 - $valor2 é $sub". "<br>";
      echo "A multiplicação de $valor1 * $valor2 é $mul". "<br>";
      echo "A divisão de $valor1 : $valor2 é $div". "<br>";
+     //respostaprof
+     $x= 4;
+     $y= 5;
+     echo "a soma de $x e $y é : " . $x + $y ;
+     echo "<br>";
+     echo "a subtração de $x e $y é : " . $x - $y ;
+     echo "<br>";
+     echo "a multiplicação de $x e $y é : " . $x * $y ;
+     echo "<br>";echo "a soma de $x e $y é : " . $x + $y ;
+     echo "<br>";
+     echo "a divisão de $x e $y é : " . $x / $y ;
+     echo "<br>";
   
     //Q4
      $numero1 = 0; 
@@ -34,7 +46,19 @@
        echo "$numero1 é menor que $numero2";}
     else
        echo "$numero1 e $numero2 são iguais" . "<br>";
-    
+    //respostaprof
+    function comparativo($a,$b){
+        if($a > $b){
+            echo "$a é maior que $b";
+            echo "<br>";
+        }elseif($a <$b){
+            echo "$a é menor que $b";
+            echo "<br>";
+        }else{
+            echo "$a é igual a $b";
+            echo"<br>";
+        }
+    }comparativo($x,$y);  // usou a variavel do exercicio 3
     
     
     
@@ -52,15 +76,59 @@
 
 //    echo "<br>";
   
-     echo (str_contains($palavra, "a")  && str_contains($palavra, "o")  ) ? "Contêm as letras" : "Não contêm as letras";
-    
+     echo (str_contains($palavra, "a")  && str_contains($palavra, "o")  ) ? "Contêm as letras" : "Não contêm as letras". "<br>";
+
+
+    // respostaprof
+    function verificarLetras($texto){
+        if(str_contains($texto, 'a')&& str_contains($texto,'o')){
+            echo "Contem";
+            echo"<br>";
+        }else{
+            echo "Não contem";
+            echo"<br>";
+        }
+    }    verificarLetras("alo");
     
     //Q6
-    
-
+    function retornaSi($algo){
+        return $algo;
+    }
+     echo retornaSi('oi');
+     echo "<br>";
 
 
 //     //07
+function verificaSenha($senha){
+    $temMinuscula = false;
+    $temMaiuscula = false; 
+    $temNumero = false;
+    $senha = str_split($senha);
+    if(count($senha)< 8){
+        echo "Menor que 8 caracteres <br>";
+        return false;
+    }
+    foreach($senha as $caracteres){
+        if (ctype_upper($caracteres)){
+            $temMaiuscula =true;
+
+        }
+        if(ctype_lower($caracteres)){
+            $temMinuscula = true;
+        }
+   if(ctype_digit($caracteres)){
+        $temNumero = true;
+    }
+
+}if ($temMaiuscula && $temMinuscula && $temNumero){
+    return true;
+} 
+}
+if(verificaSenha('4sdfGhjk')){
+    echo "Valida <br>";
+}else{
+    echo "Invalida <br>";
+}
 
    
      
@@ -96,45 +164,53 @@
 
          }
       }
+      foreach($alunos as $aluno =>$nota){
+        if($nota >= 7){
+            echo "$aluno <br>";
+        }
+      }
      //10
-    // //Q11
-    // $valor=10
-//     $arrayPares=[];
-//     function pares($valor){
-//         for(i = 0 ; i<= $valor;i++){
-//             if(i % $valor = 0){
-//                 $arrayPares_push[i];
-//             }
-//         }
-//     }
-//      echo pares(10);
-// echo $arrayPares;
-//q11
+ function areaCirculo($raio){
+    return pi()* ($raio *$raio);
+ }
+echo round(areaCirculo(4),2);
+echo "<br>";
 
-//  $arrayPares=array()
-//  function Pares($numero){
-//       for($i = 0;$i <= $numero;$i++){
-//        if($i % 2 == 0){
-      
-//       $arrayPares_push($i);
-//      $arrayPares =  Pares(10) ;
-//      return $arrayPares=array();
-//  } 
 
-//  }  
-//  }     echo var_dump($arrayPares);
 
+
+    //Q11
+function geraArray($termo){
+    $arrayPares = array ();
+    for($i = 0; $i<= $termo; $i++){
+        if($i % 2 == 0){
+            array_push($arrayPares,$i);
+        }
+    }return $arrayPares;
+}
+echo "<pre>";
+var_dump(geraArray(10));
+echo "<pre>";
+
+//Q12
+function seculo($ano){
+    return ceil($ano/100);
+}
+echo "1950 é do século: ". seculo(1950) . "<br>"; // 1950/100 = 19.5 arredonda pra cima 20
+echo "2005 é do século: " . seculo(2005) . "<br>"; // 2005/100 = 20.05 arredonda pra cima 21
+echo "1900 é do século: "  . seculo(1900)  . "<br>"; // 1900 /100 = 19 nao precisa arredondar
 
 //Q13
     echo "<br>";
  function palindromo($nova){
      if(strrev($nova)=== $nova){
-        echo "É um palíndromo";
+        echo "É um palíndromo <br>";
         
      }else{
-        echo "Não é um palíndromo";
+        echo "Não é um palíndromo <br>";
     }
  }      palindromo("arara");
-  
+        palindromo("senac");
+        palindromo("ana");
     
 ?>
